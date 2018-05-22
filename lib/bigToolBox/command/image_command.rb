e463b6827f3cb70e91772778a7fec211c1876db7
Serena_R <lirui050716@163.com>
2018-05-22 14:29:19 +0800

    [ADD] add command which image content analyze.

--- lib/bigToolBox/command/image_command.rb
+++ lib/bigToolBox/command/image_command.rb
@@ -1,17 +1,29 @@
-require 'bigToolBox/util/image_analyze_util'
+require 'bigToolBox/util/image/name_analyze_util'
+require 'bigToolBox/util/image/diff_analyze_util'
 
 module BigToolBox
 
   def self.image_resource_command
     desc "Detect image resource"
-      command :image do |c|
-        c.desc "Detect image resource whether is duplicated."
-        c.command :detect do |detect|
-          detect.action do |global_options, options, args|
+      command :image do | c |
+        c.desc "Detect duplicate name image resources."
+        c.command :name do | name |
+          name.action do | global_options, options, args |
             path = File.expand_path(global_options[:path])
             ImageAnalyzeUtil.get_duplicate_name_file_with_type(path, ".png")
           end
         end
+
+      c.desc "Detect duplicate content image resources."
+      c.command :content do | content |
+        content.action do | global_options, options, args |
+          path = File.expand_path(global_options[:path])
+          #test file name
+          file_path1 = '/Users/serena_rui/Downloads/111/image_close_bapin@4x.png'
+          file_path2 = '/Users/serena_rui/Downloads/111/image_close_bapin@3x.png'
+          AnalyzeUtil.image_diff_analyze(file_path1,file_path1)
+        end
       end
+    end
   end
 end

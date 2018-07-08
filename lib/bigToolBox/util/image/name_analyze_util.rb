8027d87f2c9f5af8be62fabe06438a6b624d7510
Serena_R <lirui050716@163.com>
2018-07-08 22:50:54 +0800

    [CHG] fix

--- lib/bigToolBox/util/image/name_analyze_util.rb
+++ lib/bigToolBox/util/image/name_analyze_util.rb
@@ -21,17 +21,17 @@ module BigToolBox
 
     def self.get_duplicate_content_file_with_type(path, file_type)
       file_filter_dic = {}
-      Dir.chdir(home_path) do
-        `tree -J #{home_path} > fileHierarchy.json`
+      Dir.chdir(path) do
+        `tree -J #{path} > fileHierarchy.json`
       end
       json_data = File.read("#{path}/fileHierarchy.json")
       result_array = JSON.parse(json_data)
 
       result_array.select do | dic |
-        detect_file_by_size(dic, PictureFileType.type_name(type), home_path, file_filter_dic)
+        FileScanUtil.detect_file_by_size(dic, PictureFileType.type_name(file_type), path, file_filter_dic)
       end
-      array = get_same_file(file_filter_dic)
-      file = File.new("#{home_path}/test.txt", 'w')
+      array = FileScanUtil.get_same_file(file_filter_dic)
+      file = File.new("#{path}/test.txt", 'w')
       file << array.to_json
       file.close
       p array
